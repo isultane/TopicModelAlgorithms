22ccc5a11ac4d6b2f95f3bf09cbfad4229181c5c
isultane <to_sultan@yahoo.com>
2015-09-28 02:26:47 +0300

    Updated GLDA

--- src/models/GibbsSamplingGLDA.java
+++ src/models/GibbsSamplingGLDA.java
@@ -16,6 +16,8 @@ public class GibbsSamplingGLDA {
 
 	/**
 	 * Gipps sampling equations, given by the model's author
+	 * For i^th token: p(z_i = j, x_i|...) \propto p(w_i|z_i=j,x_i) * p(x_i | z_i=j) * p(z_i =j | d). 
+	 * The equations for this model are quite similar to that of LDA, except we have the binary x_i variable.
 	 * p(z_i = j | d) = (Number of tokens with topic j in doc d + dAlpha) / (Number of tokens in doc d + dAlpha*K)
 	 * p(x_i = 1| z) = tau and p(x_i=0|z) = 1- tau  (I tried to learn these values but, if I remember correctly, that didn't help)
 	 * p(w_i | z_i=j, x_i=1) = (Number of word tokens of word wi with seed topic j + dMu) / (Number of seed tokens in topic j + dMu*W) 
